4e6556430e1ffa4a5f52e130b948af8d58ba2f15
Christopher Milan <chris.m.milan@gmail.com>
2019-08-18 16:10:21 -0400

    Add test option to config

--- server/src/gpio.js
+++ server/src/gpio.js
@@ -40,11 +40,19 @@ module.exports = {
     compressorIO = new Gpio(app.locals.compPin, "out");
   },
   initACJob(app) {
-    getAverageTemperature(app) > app.locals.target ? ac : off;
-    scheduleJob("0/10 * * * *", () => {
-      // Runs every 10 minutes
-      getAverageTemperature(app) > app.locals.target ? ac : off;
-    });
+    getAverageTemperature(app) > app.locals.target ? ac() : off();
+    if (app.locals.test) {
+      console.log("Test mode on");
+      setInterval(() => {
+        console.log("AC JOB CHECK");
+        getAverageTemperature(app) > app.locals.target ? ac() : off();
+      }, 1000);
+    } else {
+      scheduleJob("0/10 * * * *", () => {
+        // Runs every 10 minutes
+        getAverageTemperature(app) > app.locals.target ? ac() : off();
+      });
+    }
   },
   getFan,
   getCompressor,
